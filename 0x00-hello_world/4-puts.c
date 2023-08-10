55c1307ccc2f994edc83264f8cae59c661cd9743
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-10 14:40:28 -0700

    fifth C project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Reture: Always 0 (Success)
  */
-int main(void)
+int main(void)/*print "Programming is like building a multilingual puzzle*/
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
