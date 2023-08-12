3b7ffd3cf8c40523b213b2afef4f254c390a8bdb
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-11 18:51:36 -0700

    condition project 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : 'write the alphabet in lower case'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
