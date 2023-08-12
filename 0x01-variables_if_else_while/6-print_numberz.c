ae8ff19ba517945079ed7ff7584d918fb42d5f73
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-11 19:45:03 -0700

    condition project 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : 'print all digit from 0 to 9'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
