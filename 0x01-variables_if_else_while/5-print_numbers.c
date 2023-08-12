9744460e15af7d92d120d855aff5ec7536e78f31
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-11 19:32:06 -0700

    condition project 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : 'print sing digit from 0 to 9'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
