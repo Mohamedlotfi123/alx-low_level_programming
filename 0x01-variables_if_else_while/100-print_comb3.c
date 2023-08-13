4efa8aae3e24b618732809fc8c53e3e8b681daca
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-13 10:55:26 -0700

    advance 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : 'print combinition of two digit'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for  (y = 1+x; y <= 57; y++)
+		{
+			if (x != y)
+			{
+				putchar(x);
+				putchar(y);
+				if (x == 56 && y == 57)
+				{
+					continue;
+				}
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
