682f0601870f5b6b888fff7b2386839959fd628b
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-13 12:58:34 -0700

    advance 3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description : 'combination of two digit twice'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z, g;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 8; y++)
+		{
+			for (z = 0; z <= 9; z++)
+			{
+				for (g = 0; g <= 9; g++)
+				{
+					if (z == 0 && g == 0)
+					{
+						continue;
+					}
+					putchar(x + 48);
+					putchar(y + 48);
+					putchar(' ');
+					putchar(z + 48);
+					putchar(g + 48);
+					if (x == 9 && y == 8 && z == 9 && g == 9)
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		
+	}
+	putchar('\n');
+	return (0);
+}
+
