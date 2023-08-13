243dd5fa8398a0e4228f48964bcbf01da52c9a0f
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-13 07:17:10 -0700

    combinition

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : 'print single combinition of single digit'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+		if (x < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('$');
+		}
+	}
+	return (0);
+}
