9b3914b62d240e9f4a3a46620d133abab42f35a3
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-15 11:06:11 -0700

    function 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+	return;
+}
