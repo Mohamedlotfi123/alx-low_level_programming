0bdae1883addf95e02ccfb20d67d232cf683d025
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-16 15:48:51 -0700

    function advance

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,46 @@
 #include "main.h"
+#include<stdio.h>
 /**
  * print_to_98 -  prints all natural numbers from n to 98
+ *@n : variable
  *
  * Return: integer
  */
 void print_to_98(int n)
 {
+	int x;
+
+	if (n < 98)
+	{
+		for (x = n; x <= 98; x++)
+		{
+			if (x == 98)
+			{
+				printf("%d\n",x);
+			}
+			else
+			{
+				printf("%d, ",x);
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (x = n; x >= 98; x--)
+		{
+			if (x == 98)
+			{
+				printf("%d\n",x);
+			}
+			else
+			{
+				printf("%d, ",x);
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d\n",n);
+	}
 
 }
