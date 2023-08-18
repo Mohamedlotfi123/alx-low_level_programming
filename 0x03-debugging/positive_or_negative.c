2ce501a30e54a42fe9a81e10fecc8970539091e3
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-18 13:03:49 -0700

    Readme file

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 /**
  * positive_or_negative - test positive or negative
- *
+ *@i : variable
  * Return: Always 0 (Success)
  */
 void positive_or_negative(int i)
 {
 	if (i > 0)
 	{
-		printf("%d is positive\n",i);
+		printf("%d is positive\n", i);
 	}
 	else if (i < 0)
 	{
-		printf("%d is negative\n",i);
+		printf("%d is negative\n", i);
 	}
 	else
 	{
-		printf("%d is zero\n",i);
+		printf("%d is zero\n", i);
 	}
-}	
+}
