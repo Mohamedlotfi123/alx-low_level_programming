2e1e7fa941a6a1dd37267faad2379e66d14a91e9
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-22 13:27:52 -0700

    swap the value of two variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a : variable
+ * @b : variable
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_h
 #define MAIN_h
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif
