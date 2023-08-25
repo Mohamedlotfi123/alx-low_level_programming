1d6e8bb979a968de1633464283af83218978c1ea
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-24 18:37:15 -0700

    Capitalize all words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s : string
+ *
+ * Return : string
+ */
+char *cap_string(char *s)
+{
+	int y = 0;
+
+	while (s[y] != '\0')
+	{
+		if ((s[y] == '!' || s[y] == '(' || s[y] == ')' || s[y] == '{' || s[y] == '}' || s[y] == ';' || s[y] == '?' || s[y] == ',' || s[y] == '\t' || s[y] == '\n' || s[y] == '.' || s[y] == ' ') && s[y + 1] >= 'a' && s[y + 1] <= 'z')
+		{
+			s[y + 1] = s[y + 1] - 32;
+		}
+		y++;
+	}
+	return (s);
+}
