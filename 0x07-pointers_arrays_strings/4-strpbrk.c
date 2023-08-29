9d2474fb5ad0f1a426650faeac9484ddfa4ce408
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-08-29 15:50:18 -0700

    searches a string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,9 +10,9 @@ char *_strpbrk(char *s, char *accept)
 {
 	int x, y;
 
-	for (x = 0; s[x] >= '\0'; x++)
+	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (y = 0; accept[y] >= '\0'; y++)
+		for (y = 0; accept[y] != '\0'; y++)
 		{
 			if (s[x] == accept[y])
 			{
