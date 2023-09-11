a77ee32c39aa00d9e760fe2004b5f319b5ad3349
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-09-11 16:32:40 -0700

    Define new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - new type of data
+ * @name : pointer to char
+ * @age : float
+ * @owner : pointer to char
+ *
+ */
 struct dog
 {
 	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 #endif
