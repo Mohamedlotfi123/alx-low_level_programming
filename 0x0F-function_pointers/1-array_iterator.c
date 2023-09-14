1ac2b442e0fbbb92cf56f53b7a20914b747ea49a
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-09-13 18:07:04 -0700

    execute function gives as parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - executes function given as parameter
+ * @array : pointer to the array
+ * @size : size of the array
+ * @action : pointer to function
+ *
+ * Return: Void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	for (x = 0; x < size; x++)
+	{
+		action(array[x]);
+	}
+}
