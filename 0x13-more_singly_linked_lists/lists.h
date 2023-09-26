a4d57b7471e5e15e514b15744118190c15025910
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-09-26 15:10:59 -0700

    link list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
-#define LIST_H
-
+#define LISTS_H
 #include<stddef.h>
 
 /**
@@ -13,8 +12,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
