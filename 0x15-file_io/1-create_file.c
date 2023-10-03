9a8f1c6510d90fe1eecea7199766f37543809127
Mohamedlotfi123 <mohamed.lo.otfi123@gamil.com>
2023-10-02 17:29:33 -0700

    creates file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content : content of the file
+ *
+ * Return: 1 if success or -1 if fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fp, x = 0;
+
+	fp = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (!filename)
+	{
+		return (-1);
+	}
+	if (fp == -1)
+	{
+		return (-1);
+	}
+	while (text_content[x] != '\0')
+	{
+		x++;
+	}
+	if (x == 0)
+	{
+		exit (0);
+	}
+	else
+	{
+		write(fp, text_content, x);
+	}
+	close(fp);
+	return (1);
+}
