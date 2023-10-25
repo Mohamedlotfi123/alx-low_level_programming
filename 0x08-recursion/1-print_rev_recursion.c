6f3e382475fce0e8f68da0716ee00d909c6ca412
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2023-10-25 12:49:45 -0700

    reverse string using recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev-recursion - print string in reverse
+ * @s : string pointer
+ *
+ * Return: Void
+ */
+void _print_rev_recursion(char *s)
+{
+	char *ptr = s;
+
+	if (*ptr != '\0')
+	{
+		_print_rev_recursion(ptr + 1);
+		_putchar(*ptr);
+	}
+}
