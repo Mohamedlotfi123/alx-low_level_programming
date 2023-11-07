96a79e583776a1544d829fee00489040d9da15f3
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2023-11-06 17:15:36 -0800

    Struct Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure with three variables
+ * @name : name of the dog
+ * @age : It's age
+ * @owner : It's owner
+ *
+ * Description: Struct for the dog Id
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
