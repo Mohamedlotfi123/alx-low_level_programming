ac3639e8ec600cc438798f15a23954df0c8d3fb1
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2023-11-16 09:43:06 -0800

    link list 3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,58 @@
+#include"lists.h"
+#include <string.h>
+/**
+ * _strlen : count the length o the string
+ * @s : string pointer
+ *
+ * Return: Length of the string
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i = 0;
+
+	if (s == NULL)
+	{
+		return (0);
+	}
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * add_node : adds new node at the beginning
+ * @head : Pointer to first element
+ * @str : value of the node
+ *
+ * Return: address of the new elemet or NULL if fail
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp;
+
+	if (*head == NULL)
+	{
+		*head = malloc(sizeof(list_t));
+		if (*head == NULL)
+		{
+			return (NULL);
+		}
+		(*head)->str = strdup(str);
+		(*head)->len = _strlen(str);
+		(*head)->next = NULL;
+	}
+	else
+	{
+		tmp = malloc(sizeof(list_t));
+		if (tmp == NULL)
+		{
+			return (NULL);
+		}
+		tmp->str = strdup(str);
+		tmp->len = _strlen(str);
+		tmp->next = *head;
+		*head = tmp;
+	}
+	return (*head);
+}
