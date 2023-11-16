90181ab144d2cb853863537f7d764447dd793d15
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2023-11-16 11:00:28 -0800

    link list 5

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_list - frees the list
+ * @head : list pointer
+ *
+ * Return: Void
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr, *tmp;
+
+	if (head == NULL)
+	{
+		exit(0);
+	}
+	ptr = head;
+	while (ptr != NULL)
+	{
+		tmp = ptr->next;
+		free(ptr);
+		ptr = tmp;
+	}
+}
