c187b9c639ee552d00491c6113d52fc9218082f6
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2023-11-21 13:21:07 -0800

    frees list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees list
+ * @head : pointer to pointer to the list
+ *
+ * Return: Void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	ptr = *head;
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		free(*head);
+		*head = ptr;
+	}
+	*head = NULL;
+}
