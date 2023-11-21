128d5f9c918ce5feaac5224d2e7b230da723302c
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2023-11-21 15:41:35 -0800

    inserts a node at given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		ptr = ptr->next;
 		count++;
 	}
-	if (idx > count - 1)
+	if (idx > count)
 	{
 		return (NULL);
 	}
