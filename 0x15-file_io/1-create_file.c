9df1b07b92af3ba2de17c2074a149ff5ed7a7bb6
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2023-11-27 06:06:59 -0800

    trying

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * create_file  - creates file
+ * @filename : name of the file
+ * @text_content : string to write to the file
+ *
+ * Return: 1 on success or -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fp, i;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fp < 0)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		close(fp);
+		return (1);
+	}
+	else
+	{
+		i = write(fp, text_content, sizeof(text_content));
+		if (i < 0)
+		{
+			return (-1);
+		}
+	}
+	close(fp);
+	return (1);
+}
