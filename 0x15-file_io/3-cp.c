2cb7e10eb4287db5a5b561e4c7d319cc6dda5116
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2023-11-27 18:02:01 -0800

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,38 +13,33 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(1, "Usage: cp file_from file_to");
-		write(2, "\n", 1);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd_1 = open(argv[1], O_RDONLY);
 	i = read(fd_1, buf, 1024);
 	if (fd_1 < 0 || i < 0)
 	{
-		dprintf(1, "Error: Can't read to %s", argv[1]);
-		write(2, "\n", 1);
+		dprintf(2, "Error: Can't read to %s\n", argv[1]);
 		exit(98);
 	}
 	fd_2 = open(argv[2], O_WRONLY | O_CREAT, 0664);
 	x = write(fd_2, buf, i);
 	if (fd_2 < 0 || x < 0)
 	{
-		dprintf(1, "Error: Can't write to %s", argv[2]);
-		write(2, "\n", 1);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	y = close(fd_1);
 	if (y < 0)
 	{
-		dprintf(1, "Error: Can't close fd %d", fd_1);
-		write(2, "\n", 1);
+		dprintf(2, "Error: Can't close fd %d\n", fd_1);
 		exit(100);
 	}
 	y = close(fd_2);
 	if (y < 0)
 	{
-		dprintf(1, "Error: Can't close fd %d", fd_1);
-		write(2, "\n", 1);
+		dprintf(2, "Error: Can't close fd %d\n", fd_1);
 		exit(100);
 	}
 	return (0);
