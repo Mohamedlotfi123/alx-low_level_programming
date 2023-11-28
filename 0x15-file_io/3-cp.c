c6840d5b556e88741bd9922f274e21fa1bb78705
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2023-11-27 18:13:05 -0800

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,9 +6,10 @@
  *
  * Return: Void
  */
+int main(int argc, char *argv[]);
 int main(int argc, char *argv[])
 {
-	int fd_1, fd_2, i, x, y;
+	int fd_1, fd_2, i, y;
 	char buf[1024];
 
 	if (argc != 3)
@@ -17,18 +18,29 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	fd_1 = open(argv[1], O_RDONLY);
-	i = read(fd_1, buf, 1024);
-	if (fd_1 < 0 || i < 0)
+	if (fd_1 < 0)
 	{
 		dprintf(2, "Error: Can't read to %s\n", argv[1]);
 		exit(98);
 	}
 	fd_2 = open(argv[2], O_WRONLY | O_CREAT, 0664);
-	x = write(fd_2, buf, i);
-	if (fd_2 < 0 || x < 0)
+	if (fd_2 < 0)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
+	}
+	while ((i = read(fd_1, buf, 1024)) > 0)
+	{
+		if (write(fd_2, buf, 1024) != i)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (i < 0)
+	{
+		dprintf(2, "Error: Can't read to %s\n", argv[1]);
+		exit(98);
 	}
 	y = close(fd_1);
 	if (y < 0)
