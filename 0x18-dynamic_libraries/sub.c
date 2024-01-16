583bfb13e378fdf6b4981185b53feaa229dc9857
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2024-01-15 17:44:42 -0800

    C call from python

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * sub - subtract two integers
+ * @a : first integer
+ * @b : second integer
+ *
+ * Return: Subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
