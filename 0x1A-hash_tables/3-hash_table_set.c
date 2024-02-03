a07bb0fe715997c7646b86eb9d0998eb05d29691
root <root@DESKTOP-8VTPROF>
2024-02-02 16:35:58 -0800

    add element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - function adds an element to the hash table
+ * @ht : Hash table you want to update or add the key/value
+ * @key : key of the value
+ * @value : value of the key
+ *
+ * Return: 1 if succeed, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long x;
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	node->key = malloc(strlen(key) + 1);
+	node->value = malloc(strlen(value) + 1);
+	if (node->key == NULL || node->value == NULL)
+	{
+		return (1);
+	}
+	if (node == NULL)
+	{
+		return (1);
+	}
+	x = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[x] == NULL)
+	{
+		strcpy(node->key, key);
+		strcpy(node->value, value);
+		node->next = NULL;
+		ht->array[x] = node;
+	}
+	else
+	{
+		if (strcmp(key, ht->array[x]->key) == 0)
+		{
+			ht->array[x]->key = (char *)key;
+			ht->array[x]->value = (char *)value;
+			free(node);
+		}
+		else
+		{
+			strcpy(node->key, key);
+			strcpy(node->value, value);
+			node->next = ht->array[x];
+			ht->array[x] = node;
+		}
+	}
+	return (0);
+}
