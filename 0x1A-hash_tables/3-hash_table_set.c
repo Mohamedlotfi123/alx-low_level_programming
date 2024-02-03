9bce789972b96c779d97a377d95e08ad009891bf
root <root@DESKTOP-8VTPROF>
2024-02-02 17:20:11 -0800

    add element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,44 +7,50 @@
  *
  * Return: 1 if succeed, 0 otherwise
  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long x;
 	hash_node_t *node;
 
 	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (1);
 	node->key = malloc(strlen(key) + 1);
 	node->value = malloc(strlen(value) + 1);
 	if (node->key == NULL || node->value == NULL)
-	{
 		return (1);
-	}
-	if (node == NULL)
-	{
-		return (1);
-	}
+	strcpy(node->key, key);
+	strcpy(node->value, value);
 	x = key_index((const unsigned char *)key, ht->size);
-	if (!ht->array[x])
+	if (ht->array[x] == NULL)
 	{
-		strcpy(node->key, key);
-		strcpy(node->value, value);
 		node->next = NULL;
 		ht->array[x] = node;
 	}
 	else
 	{
-		if (strcmp(key, ht->array[x]->key) == 0)
+		if (strcmp((ht->array[x])->key, key) == 0)
 		{
-			ht->array[x]->key = (char *)key;
-			ht->array[x]->value = (char *)value;
+			strcpy((ht->array[x])->value, value);
+			free(node->key);
+			free(node->value);
 			free(node);
 		}
 		else
 		{
-			strcpy(node->key, key);
-			strcpy(node->value, value);
-			node->next = ht->array[x];
-			ht->array[x] = node;
+			if (node->next == NULL)
+			{
+				node->next = ht->array[x];
+				ht->array[x] = node;
+			}
+			else
+			{
+				while (node->next != NULL)
+					node = node->next;
+				node->next = ht->array[x];
+				ht->array[x] = node;
+			}
 		}
 	}
 	return (0);
