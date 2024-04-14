198491974c66a5d4704828a88ffadd2959163182
Mohamedlotfi123 <mohamed.lo.otfi123@gmail.com>
2024-04-14 10:56:49 -0700

    functions that adds to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht : the hash table you want to add or update the key/value.
+ * @key : the key
+ * @value : the value associated with the key.
+ *
+ * Return: 1 if succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int index;
+	hash_node_t *node, *ptr;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht != NULL)
+	{
+		if (ht->array[index] == NULL)
+		{
+			node->key = (char *)key;
+			node->value = (char *)value;
+			node->next = NULL;
+			ht->array[index] = node;
+		}
+		else
+		{
+			if (strcmp(ht->array[index]->key, key) == 0)
+			{
+				ht->array[index]->value = (char *)value;
+				free(node);
+			}
+			else
+			{
+				ptr = ht->array[index];
+				while (ptr->next != NULL)
+					ptr  = ptr->next;
+				node->key = (char *)key;
+				node->value = (char *)value;
+				node->next = NULL;
+				ptr->next = node;
+			}
+		}
+	}
+	return (1);
+}
