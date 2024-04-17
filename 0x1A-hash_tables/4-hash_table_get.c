8055469301a251249d752b570476a2e5ac9c9510
root <root@DESKTOP-8VTPROF>
2024-04-17 14:48:46 +0200

    function retrieves an element from hash tabel

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,17 +11,20 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *ptr;
 
-	index = key_index((const unsigned char *)key, ht->size);
-	ptr = ht->array[index];
-	if (ptr != NULL)
+	if (key == NULL)
 	{
-		if (strcmp(ptr->key, key) == 0)
-			return (ptr->value);
-		while (ptr)
+		index = key_index((const unsigned char *)key, ht->size);
+		ptr = ht->array[index];
+		if (ptr != NULL)
 		{
 			if (strcmp(ptr->key, key) == 0)
 				return (ptr->value);
-			ptr = ptr->next;
+			while (ptr)
+			{
+				if (strcmp(ptr->key, key) == 0)
+					return (ptr->value);
+				ptr = ptr->next;
+			}
 		}
 	}
 	return (NULL);
